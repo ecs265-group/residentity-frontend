8427783eb0ac7570e90c2d8298e84deaa6447bc2
AnishK10 <anish.21911105@viit.ac.in>
2024-12-08 16:36:29 -0800

    fix: 🐛 Changed the success message after registration
    Changed the error message type to success after successful registration

--- app/register/page.js
+++ app/register/page.js
@@ -21,6 +21,7 @@ export default function Register() {
   const [pageLoading, setPageLoading] = useState(true);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [success, setSuccess] = useState(null);
   const [buttonEnabled, setButtonEnabled] = useState(false);
 
   useEffect(() => {
@@ -44,16 +45,11 @@ export default function Register() {
     } else {
       setButtonEnabled(true);
     }
-  }, [registerForm.email, registerForm.password, registerForm.verifyPassword]);
+  }, [registerForm]);
 
-  const validateEmail = (email) => {
-    const re = /\S+@\S+\.\S+/;
-    return re.test(email);
-  };
+  const validateEmail = (email) => /\S+@\S+\.\S+/.test(email);
 
-  const validatePassword = (password) => {
-    return password.length >= 8;
-  };
+  const validatePassword = (password) => password.length >= 8;
 
   const handleInputChange = (e) => {
     const { id, value } = e.target;
@@ -67,7 +63,9 @@ export default function Register() {
     e.preventDefault();
     setLoading(true);
     setError(null);
+    setSuccess(null);
     const publicKey = (await generateKeypair(registerForm.password)).publicKey;
+
     try {
       const res = await axios.post(
         process.env.NEXT_PUBLIC_API_URL + "/api/register",
@@ -88,18 +86,20 @@ export default function Register() {
           },
         }
       );
+
       if (res.status === 200) {
-        setError("Registration successful. Redirecting to login page...");
-        setTimeout(() => {
-          router.push("/login");
-        }, 3000);
+        setSuccess("Registration successful! Redirecting to login page...");
+        setTimeout(() => router.push("/login"), 3000);
       } else {
-        const data = res.data;
-        setError(data.error?.message || "An error occurred. Please try again later.");
+        setError(
+          res.data.error?.message ||
+            "An error occurred. Please try again later."
+        );
       }
     } catch (error) {
       setError(
-        error.response?.data?.error?.message || "An error occurred. Please try again later."
+        error.response?.data?.error?.message ||
+          "An error occurred. Please try again later."
       );
     } finally {
       setLoading(false);
@@ -108,20 +108,24 @@ export default function Register() {
 
   if (pageLoading) {
     return (
-      <div className='flex justify-center mt-2'>
+      <div className="flex justify-center mt-2">
         <Loader />
       </div>
     );
   }
 
   return (
-    <div className='mt-20 mx-auto w-1/3 flex flex-col items-center justify-center text-center border bg-c0 gap-4 rounded-lg border-white/15 p-6'>
-      <h1 className='text-3xl text-white font-semibold p-4'>Register</h1>
-      <form className='w-2/3 flex flex-col gap-2'>
+    <div className="mt-20 mx-auto w-1/3 flex flex-col items-center justify-center text-center border bg-c0 gap-4 rounded-lg border-white/15 p-6">
+      <h1 className="text-3xl text-white font-semibold p-4">Register</h1>
+      <form className="w-2/3 flex flex-col gap-2">
         {[
           { id: "email", type: "email", placeholder: "Email" },
           { id: "password", type: "password", placeholder: "Password" },
-          { id: "verifyPassword", type: "password", placeholder: "Re-enter Password" },
+          {
+            id: "verifyPassword",
+            type: "password",
+            placeholder: "Re-enter Password",
+          },
           { id: "firstName", type: "text", placeholder: "First Name" },
           { id: "lastName", type: "text", placeholder: "Last Name" },
           { id: "organization", type: "text", placeholder: "Organization" },
@@ -129,14 +133,17 @@ export default function Register() {
           { id: "state", type: "text", placeholder: "State" },
           { id: "countryCode", type: "text", placeholder: "Country Code" },
         ].map((field) => (
-          <div className='flex flex-col' key={field.id}>
-            <label htmlFor={field.id} className='flex text-c2 text-sm font-medium'>
+          <div className="flex flex-col" key={field.id}>
+            <label
+              htmlFor={field.id}
+              className="flex text-c2 text-sm font-medium"
+            >
               {field.placeholder}
             </label>
             <input
               type={field.type}
               id={field.id}
-              className='p-2 rounded-md text-white bg-c0 border border-white/15 placeholder-c2/50 text-sm focus:outline-none'
+              className="p-2 rounded-md text-white bg-c0 border border-white/15 placeholder-c2/50 text-sm focus:outline-none"
               placeholder={field.placeholder}
               value={registerForm[field.id]}
               onChange={handleInputChange}
@@ -147,21 +154,26 @@ export default function Register() {
         <button
           disabled={!buttonEnabled}
           className={`h-9 px-4 py-2 my-1 w-full flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors
-							focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-white border
-							bg-c1 text-white border-c2 cursor-pointer
-							hover:bg-white hover:text-c0
-							disabled:bg-c1 disabled:text-c2 disabled:cursor-not-allowed`}
+            focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-white border
+            bg-c1 text-white border-c2 cursor-pointer
+            hover:bg-white hover:text-c0
+            disabled:bg-c1 disabled:text-c2 disabled:cursor-not-allowed`}
           onClick={(e) => handleSubmit(e)}
         >
           Register
         </button>
       </form>
-      {loading ? (
-        <div className='flex justify-center mt-2'>
+      {loading && (
+        <div className="flex justify-center mt-2">
           <Loader />
         </div>
-      ) : null}
-      {error ? <div className='text-red-500/90 text-sm text-center'>{error}</div> : null}
+      )}
+      {success && (
+        <div className="text-green-500/90 text-sm text-center">{success}</div>
+      )}
+      {error && (
+        <div className="text-red-500/90 text-sm text-center">{error}</div>
+      )}
     </div>
   );
 }
